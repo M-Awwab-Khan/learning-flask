bdd8c743f3a2070369c1cc194ff5736d488e8a6e
Awwab <mawwabkhank2006@gmail.com>
2024-02-12 20:15:01 +0500

    used additional decorators (practice)

--- main.py
+++ main.py
@@ -2,6 +2,24 @@ from flask import Flask
 
 app = Flask(__name__)
 
+def make_bold(function):
+    def wrapper():
+        text = function()
+        return f'<b>{text}</b>'
+    return wrapper
+
+def make_italic(function):
+    def wrapper():
+        text = function()
+        return f'<em>{text}</em>'
+    return wrapper
+
+def make_underlined(function):
+    def wrapper():
+        text = function()
+        return f'<u>{text}</u>'
+    return wrapper
+
 @app.route('/')
 def hello():
     return '<h1 style="text-align: center">Hello World</h1>'\
@@ -9,6 +27,9 @@ def hello():
             '<img src="https://media.giphy.com/media/ukgcHmvflVWjwE8eU5/giphy.gif?cid=790b7611445wpyc5ewpievp0xxxi3922z1bne2bbvu8x3lqx&ep=v1_gifs_search&rid=giphy.gif&ct=g", width=200>'
 
 @app.route('/bye')
+@make_bold
+@make_italic
+@make_underlined
 def say_bye():
     return 'bye'
 
